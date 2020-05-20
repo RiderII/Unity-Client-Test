5ec8d345b02509856a0511deffad06fb55f0104a
dominicsc2 <dominicsc2hs@gmail.com>
2020-05-19 22:08:21 -0500

    first commit

---
+++ exampleClient/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    // store all players info in the client side.
+    public static Dictionary<int, PlayerManager> players = new Dictionary<int, PlayerManager>();
+
+    public GameObject localPlayerPrefab;
+    public GameObject playerPrefab;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            //ensure that only one instance of this class exists makes sense for every single client you only have one
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void SpawnPlayer(int _id, string _username, Vector3 _position, Quaternion _rotation)
+    {
+        GameObject _player;
+        if (_id == Client.instance.myId)
+        {
+            _player = Instantiate(localPlayerPrefab, _position, _rotation);
+        }
+        else
+        {
+            _player = Instantiate(playerPrefab, _position, _rotation);
+        }
+
+        _player.GetComponent<PlayerManager>().id = _id;
+        _player.GetComponent<PlayerManager>().username = _username;
+        players.Add(_id, _player.GetComponent<PlayerManager>());
+    }
+}
