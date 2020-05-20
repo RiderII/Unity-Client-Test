f48de638a005daa4fb3c1112adc89c640d3fba6a
MiguelDelgado27 <miguel.9dc@gmail.com>
2020-05-20 01:32:52 -0500

    project configuration
    Api compatibility level .Net 4.x
    - Habilitar system.IO.Ports

---
+++ exampleClient/Assets/Scripts/ArduinoScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoScript : MonoBehaviour
+{
+    public float speed;
+    public float amountToMove;
+
+    SerialPort sp = new   SerialPort("COM3",9600);  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        amountToMove = speed * Time.deltaTime;
+        if (sp.IsOpen){
+            try {
+                MoveObject(sp.ReadByte());
+                print(sp.ReadByte());
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+
+    void MoveObject(int direction)
+    {
+        if(direction == 1)
+        {
+            transform.Translate(Vector3.left * amountToMove, Space.World);
+        }
+    }
+}
