5e230439a43d76fbe2c94bffc8de5af2684b893f
MiguelDelgado27 <miguel.9dc@gmail.com>
2020-06-03 14:21:06 -0500

    commit
    -little change

--- exampleClient/Assets/Scripts/PlayerController.cs
+++ exampleClient/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO.Ports;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    SerialPort sp = new SerialPort("COM2", 9600);
+
+    private void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
     private void FixedUpdate()
     {
         SendInputToServer();
+       
+        if (sp.IsOpen)
+        {
+            try
+            {
+                SendInputIot(sp.ReadByte());
+
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+
+    private void SendInputIot(int direction)
+    {
+        bool[] _inputs = new bool[] { };
+        if (direction == 1)
+        {
+           _inputs = new bool[] {true,false,false,false };
+        }
+        if (direction == 2)
+        {
+            _inputs = new bool[] { false, true, false, false };
+        }
+        if (direction == 3)
+        {
+            _inputs = new bool[] { false, false, true, false };
+        }
+        if (direction == 4)
+        {
+            _inputs = new bool[] { false, false, false, true };
+        }
+        ClientSend.PlayerMovement(_inputs);
     }
 
     private void SendInputToServer()
