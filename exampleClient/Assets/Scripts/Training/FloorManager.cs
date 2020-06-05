f00884329200c56b8ceedf616da6321e62cb01be
MiguelDelgado27 <miguel.9dc@gmail.com>
2020-06-04 20:21:19 -0500

    folder order
    .

---
+++ exampleClient/Assets/Scripts/Training/FloorManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    public GameObject[] floors;
+    public Player player;
+    public float floorSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject floor in floors) {
+            if (floor.transform.position.z + floorSize < player.transform.parent.position.z) {
+                floor.transform.position = new Vector3(floor.transform.position.x, floor.transform.position.y, floor.transform.position.z +
+                    floorSize * floors.Length);
+            }
+        }
+    }
+}
---
+++ exampleClient/Assets/Scripts/Training/ToggleMusic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Toggle m_Toggle;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+        Debug.Log(audioSource);
+        //m_Toggle = GetComponent<Toggle>();
+        //Debug.Log(m_Toggle);
+
+    }
+
+   public void toggleOnclick(Toggle m_Toggle)
+   {
+        if (m_Toggle.isOn)
+        {
+            Debug.Log("is on");
+
+            audioSource.Pause();
+        }
+        else
+        {
+            Debug.Log("is off");
+
+            audioSource.Play(0);
+            
+        }
+   }
+}
