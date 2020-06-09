4913275180af71169be772ae8d4e3aaf531db28a
dominicsc2 <dominicsc2hs@gmail.com>
2020-06-09 18:44:09 -0500

    can look up without taking the bike

--- exampleClient/Assets/Game Mode/SinglePLayer/Scripts/CameraMouse.cs
+++ exampleClient/Assets/Game Mode/SinglePLayer/Scripts/CameraMouse.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class CameraMouse : MonoBehaviour
 {
+
     public float mouseSensitivity = 100.0f;
     public float clampAngle = 80.0f;
-    private float rotY = 0.0f; // rotation around the up/y axis
     private float rotX = 0.0f; // rotation around the right/x axis
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 rot = transform.localRotation.eulerAngles;
-        rotY = rot.y;
         rotX = rot.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = -Input.GetAxis("Mouse Y");
-        rotY += mouseX * mouseSensitivity * Time.deltaTime;
-        rotX += mouseY * mouseSensitivity * Time.deltaTime;
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-        transform.rotation = localRotation;
+        float _mouseHorizontal = Input.GetAxis("Mouse X");
+        rotX += _mouseHorizontal * mouseSensitivity * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(0f, rotX, 0f);
     }
 }
