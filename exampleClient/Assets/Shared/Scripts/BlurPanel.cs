a897a75c6c545e59d2340e0d4ae0f7ac9e9f57f0
dominicsc2 <dominicsc2hs@gmail.com>
2020-06-14 14:25:07 -0500

    added blur effect for player canvas. Looks cute

---
+++ exampleClient/Assets/Shared/Scripts/BlurPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(CanvasGroup))]
+public class BlurPanel : Image
+{
+    public bool animate;
+    public float time = 1.5f;
+    public float delay = 0f;
+
+    CanvasGroup canvas;
+
+    protected override void Reset()
+    {
+        color = Color.black * 1f;
+    }
+
+    protected override void Awake()
+    {
+        canvas = GetComponent<CanvasGroup>();
+    }
+
+    protected override void OnEnable()
+    {
+        if (Application.isPlaying)
+        {
+            material.SetFloat("_Size", 0);
+            canvas.alpha = 0;
+            LeanTween.value(gameObject, UpdateBlur, 0, 1, time).setDelay(delay);
+        }
+    }
+
+    void UpdateBlur(float value)
+    {
+        material.SetFloat("_Size", value);
+        canvas.alpha = value;
+    }
+}
