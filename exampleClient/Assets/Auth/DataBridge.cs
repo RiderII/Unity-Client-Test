bd9e2362cd20d5ce18354678ab21c98eb1636fe9
MiguelDelgado27 <miguel.9dc@gmail.com>
2020-07-02 22:48:01 -0500

    viualizar jugadores y sus estadisticas
    listado de jugadores
    Visualización de estadisticas y medallas de cada jugador

--- exampleClient/Assets/Auth/DataBridge.cs
+++ exampleClient/Assets/Auth/DataBridge.cs
@@ -189,6 +189,31 @@ public class DataBridge : MonoBehaviour
 
         return lista;
     }
+    public async Task<List<string>> LoadPlayerMedals(string userid)
+    {
+        List<string> lista = new List<string>();
+        await FirebaseDatabase.DefaultInstance.GetReference("UserMedals").Child(userid).GetValueAsync()
+            .ContinueWith((task => {
+                if (task.IsCanceled)
+                {
+
+                }
+                if (task.IsFaulted)
+                {
+
+                }
+                if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    foreach (var child in snapshot.Children)
+                    {
+                        string t = child.Value.ToString();
+                        lista.Add(t);
+                    }
+                }
+            }));
+        return lista;
+    }
     public async Task<User> LoadUserProfile()
     {
         User userProfile = new User();
@@ -242,4 +267,36 @@ public class DataBridge : MonoBehaviour
 
         return users;
     }
+
+    public async Task<string> LoadPlayersRecords(string userid)
+    {
+        float calorias = 0;
+        float tiempo = 0;
+        float distancia = 0;
+        await FirebaseDatabase.DefaultInstance.GetReference("UserRecords").Child(userid).GetValueAsync().ContinueWith((task => {
+            if (task.IsCanceled)
+            {
+
+            }
+            if (task.IsFaulted)
+            {
+
+            }
+            if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                foreach (var child in snapshot.Children)
+                {
+                    string t = child.GetRawJsonValue();
+                    MapReport data = JsonUtility.FromJson<MapReport>(t);
+                    calorias += data.burned_calories;
+                    tiempo += data.totalGameTime;
+                    distancia += data.traveled_kilometers;
+                    
+                }
+            }
+        }));
+
+        return $"{calorias},{tiempo} seg,{distancia} m";
+    } 
 }
--- exampleClient/Assets/Game Mode/Multiplayer/MenuyNiveles/SearchPlayers.cs
+++ exampleClient/Assets/Game Mode/Multiplayer/MenuyNiveles/SearchPlayers.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SearchPlayers : MonoBehaviour
 {
     public GameObject userRowPrefab;
     public GameObject container;
-    
+    public GameObject searchPanel;
+    public GameObject playerView;
+
+    public TMP_InputField caloriasInput;
+    public TMP_InputField tiempoInput;
+    public TMP_InputField distanciaInput;
+
+    public GameObject medalOffPrefab;
+    public GameObject medalOnPrefab;
+    private MedalCollection medalCollection;
+
+    private void Start()
+    {
+        medalCollection = new MedalCollection();
+    }
+
     public async void SearchUsers(string usersearch)
     {
         foreach (Transform child in container.transform)
@@ -19,11 +35,50 @@ public class SearchPlayers : MonoBehaviour
             var urow = Instantiate(userRowPrefab, container.transform);
             GameObject username = urow.transform.GetChild(1).gameObject;
             GameObject email = urow.transform.GetChild(2).gameObject;
+            GameObject viewBtn = urow.transform.GetChild(3).gameObject;
 
             username.GetComponent<TextMeshProUGUI>().text = u.username;
             email.GetComponent<TextMeshProUGUI>().text = u.email;
+            Button view = viewBtn.GetComponent<Button>();
+            view.onClick.AddListener(() => ViewPLayer(u));
+
         }
     }
 
-    // m_YourThirdButton.onClick.AddListener(() => ButtonClicked(42));
+    private async void ViewPLayer(User u)
+    {
+        //pass user
+        searchPanel.SetActive(false);
+        playerView.SetActive(true);
+        var board = playerView.transform.GetChild(0).gameObject;
+        foreach (Transform child in board.transform)
+            Destroy(child.gameObject);
+        var playerName = playerView.transform.GetChild(1).gameObject.transform.GetChild(0).gameObject;
+        TMP_InputField username = playerName.GetComponent<TMP_InputField>();
+        username.text = u.username;
+
+        //load records
+        string datos = await DataBridge.instance.LoadPlayersRecords(u.ID);
+        string[] words = datos.Split(',');
+        caloriasInput.text = words[0];
+        tiempoInput.text = words[1];
+        distanciaInput.text = words[2];
+
+        //medals
+        var lista = await DataBridge.instance.LoadPlayerMedals(u.ID);
+        foreach (KeyValuePair<string, MedalSprites> entry in medalCollection.sprites)
+        {
+            if (lista.Contains(entry.Key))
+            {
+                GameObject medal = Instantiate(medalOnPrefab, board.transform);
+                medal.GetComponent<Image>().sprite = entry.Value.on;
+            }
+            else
+            {
+                GameObject medal = Instantiate(medalOffPrefab, board.transform);
+                medal.GetComponent<Image>().sprite = entry.Value.off;
+            }
+        }
+    }
+    
 }
