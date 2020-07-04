cee8a84c463f4899bb1578275e0631a2f5b44bdc
dominicsc2 <dominicsc2hs@gmail.com>
2020-07-04 18:28:42 -0500

    final result

---
+++ exampleClient/Assets/Shared/Scripts/LerpColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LerpColor : MonoBehaviour
+{
+    Image colorPanel;
+    [SerializeField] [Range(0f, 1f)] float lerpTime;
+
+    [SerializeField] Color myColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorPanel = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colorPanel.material.color = Color.Lerp(colorPanel.material.color, myColor, lerpTime);
+    }
+}
