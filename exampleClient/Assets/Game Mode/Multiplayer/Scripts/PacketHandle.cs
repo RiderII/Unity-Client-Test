1fbdeaa08160aa25f35178937165744cac57cc1a
MiguelDelgado27 <miguel.9dc@gmail.com>
2020-09-26 20:00:49 -0500

    commit test

--- exampleClient/Assets/Game Mode/Multiplayer/Scripts/PacketHandle.cs
+++ exampleClient/Assets/Game Mode/Multiplayer/Scripts/PacketHandle.cs
@@ -51,6 +51,7 @@ public class PacketHandle : MonoBehaviour
 
     public static void PlayerPosition(Packet _packet)
     {
+        PlayerManager.statisticsFrame.transform.GetChild(6).GetComponent<TextMeshProUGUI>().text = System.DateTime.Now.ToString("hh.mm.ss.ffff");
         Debug.Log(System.DateTime.Now.ToString("hh.mm.ss.ffff"));
         int _id = _packet.ReadInt();
         Vector3 _position = _packet.ReadVector3();
